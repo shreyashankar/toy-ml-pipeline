cf7e50aaf52166f1fd25446f16ddac70af5c212b
Shreya <ss.shankar505@gmail.com>
2021-03-07 17:58:32 -0800

    Modifies ingestion/cleaning and featuregen to produce for all of 2020.

--- etl/featuregen.py
+++ etl/featuregen.py
@@ -1,20 +1,29 @@
+import itertools
+import os
 import pandas as pd
 
 from utils import io, feature_generators
 
 if __name__ == '__main__':
-    # Load latest clean data
-    df = io.load_output_df('clean')
+    months = [f'{month:02d}' for month in range(1, 13)]
+    years = ['2020']
+    product = itertools.product(months, years)
 
-    # Create features and label
-    pickup_features = feature_generators.Pickup().compute(df)
-    trip_features = feature_generators.Trip().compute(df)
-    categorical_features = feature_generators.Categorical().compute(df)
-    label = feature_generators.HighTip().compute(df)
+    for month, year in product:
+        # Load latest clean data
+        clean_component = os.path.join('clean', f'{year}_{month}')
+        df = io.load_output_df(clean_component)
 
-    # Concatenate features
-    features_df = pd.concat(
-        [pickup_features, trip_features, categorical_features, label, df['tpep_pickup_datetime'].to_frame()], axis=1)
+        # Create features and label
+        pickup_features = feature_generators.Pickup().compute(df)
+        trip_features = feature_generators.Trip().compute(df)
+        categorical_features = feature_generators.Categorical().compute(df)
+        label = feature_generators.HighTip().compute(df)
 
-    # Write features to s3
-    print(io.save_output_df(features_df, 'features'))
+        # Concatenate features
+        features_df = pd.concat(
+            [pickup_features, trip_features, categorical_features, label, df['tpep_pickup_datetime'].to_frame()], axis=1)
+
+        # Write features to s3
+        features_component = os.path.join('features', f'{year}_{month}')
+        print(io.save_output_df(features_df, features_component))
