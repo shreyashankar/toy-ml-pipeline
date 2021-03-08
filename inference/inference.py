de0f739f26fe8fe842791ee2ad66da34572c15a9
Shreya <ss.shankar505@gmail.com>
2021-03-08 14:44:43 -0800

    Local inference flask app.

--- inference/inference.py
+++ inference/inference.py
@@ -1,15 +1,25 @@
-from utils import io, models
+from utils import io
 
 import pandas as pd
+import requests
 
 if __name__ == '__main__':
     # Grab latest features and model wrapper
-    df = io.load_output_df('features/2020_12')
-    mw = models.RandomForestModelWrapper.load('training/models')
+    df = io.load_output_df(
+        'features/2020_12').drop('tpep_pickup_datetime', axis=1)
 
-    # Run model on latest features
-    preds = mw.predict(df)
+    # Run model on latest features for a random example
+    url = 'http://localhost:5000/predict'
 
-    # Dump outputs
-    preds_df = pd.DataFrame({'prediction': preds})
-    print(io.save_output_df(preds_df, 'inference'))
+    while(True):
+        inp = input('Press enter to make a prediction and q to quit.\n')
+        if (inp.strip() == 'q'):
+            break
+        example = df.sample(1).to_dict('r')[0]
+        print(f'Request: {example}')
+
+        response = requests.post(
+            url, json=example)
+        print(f'Response: {response.json()}')
+
+    print('Exiting.')
