20845d75d051afbd2df4fc2263ec88d414a1e141
Shreya Shankar <ss.shankar505@gmail.com>
2021-05-01 12:13:39 -0700

    Add argparse

--- inference/inference.py
+++ inference/inference.py
@@ -15,9 +15,13 @@ import sys
 
 parser = argparse.ArgumentParser(description='Run inference.')
 parser.add_argument('--start', type=int, help='Start index for inference.', const=0, nargs='?')
+parser.add_argument('--batch_size', type=int, help='Batch size for inference.', const=256, nargs='?')
 args = parser.parse_args()
+
 global idx
+global batch_size
 idx = args.start if args.start else 0
+batch_size = args.batch_size if args.batch_size else 0
 
 # Capture index when user quits
 def signal_handler(sig, frame):
@@ -33,13 +37,15 @@ def main():
     # Run model on latest features for a random example
     prediction_url = 'http://127.0.0.1:8000/predict'
     log_url = 'http://127.0.0.1:8000/log_prediction'
-    batch_size = 256
     
     # Register signal handler
     signal.signal(signal.SIGINT, signal_handler)
+    
+    # Register globals
+    global idx
+    global batch_size  
 
-    while(True):
-        global idx        
+    while(True):      
         example = df.iloc[range(idx, idx + batch_size)]
         req = {
             'data': example.to_json(),
@@ -49,13 +55,13 @@ def main():
 
         response = requests.post(
             prediction_url, json=req)
+        idx += batch_size
         res = response.json()
         
         result_df = pd.DataFrame({'id': res['id'], 'timestamp': example.tpep_pickup_datetime.to_list(),'prediction': res['prediction'], 'label': example.high_tip_indicator.to_list()})
         result_df['correct'] = (result_df['prediction'] >= 0.5) == (result_df['label'])
         print(result_df)
         print(f"Score: {res['score']}")
-        idx += batch_size
 
     print('Exiting.')
 
