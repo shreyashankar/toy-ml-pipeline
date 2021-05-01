e60fd737a327fcb6b453a26bb57400f612180eb3
Shreya Shankar <ss.shankar505@gmail.com>
2021-05-01 11:25:29 -0700

    Final working grafana

--- inference/app.py
+++ inference/app.py
@@ -1,43 +1,30 @@
-from decimal import Decimal, ROUND_UP
 from flask import Flask, jsonify, request
 from mltrace import get_db_uri, set_db_uri, create_component, tag_component, register, create_random_ids
+from prometheus_client import Histogram
 from prometheus_flask_exporter import PrometheusMetrics
 from utils import io, models
 
 import numpy as np
 import pandas as pd
 
+# Create app
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
 
-# static information as metric
+# Log static information as metric
 metrics.info('app_info', 'Application info', version='0.1')
 
+# Load model
 global mw
 mw = models.RandomForestModelWrapper.load('training/models')
 model_path = io.get_output_path('training/models')
 
-prediction_counter = metrics.counter(
-    'prediction_counter', 'Count by predicted label',
-    labels={'prediction': lambda r: float((Decimal(r.get_json()['prediction'][0]) * 2).quantize(Decimal('.1'), rounding=ROUND_UP) / 2) }
-)
+# Create metrics
+buckets = (*(np.around(np.arange(0, 1, 0.05), 2).tolist()), float("inf"))
+prediction_histogram = Histogram(name='prediction_histogram', description='Histogram of prediction outputs', registry=metrics.registry, buckets=buckets)
 
-buckets = (*np.arange(0, 1, 0.05).tolist(), float("inf"))
-prediction_histogram = metrics.histogram(
-    'prediction_output', 'Histogram of predictions',
-    buckets=buckets,
-    labels={'prediction': lambda r: float((Decimal(r.get_json()['prediction'][0]) * 2).quantize(Decimal('.1'), rounding=ROUND_UP) / 2)}
-)
-
-# @app.route('/log_prediction', methods=['GET', 'POST'])
-# @prediction_counter
-# @prediction_histogram
-# def log_live_prediction():
-#     return jsonify({'status': 200})
-
+# Prediction function
 @app.route('/predict', methods=['POST'])
-@prediction_counter
-@prediction_histogram
 @register(component_name='inference', inputs=[model_path], input_vars=['feature_path', 'row_idx'], output_vars=['output_ids'], endpoint=True)
 def predict():
     req = request.get_json()
@@ -45,11 +32,14 @@ def predict():
     row_idx = f"row_idx_{req['row_idx']}" if 'row_idx' in req else None
 
     df = pd.read_json(req['data'])
-    # df = pd.DataFrame({k: [v] for k, v in req['data'].items()})
     df['prediction'] = mw.predict(df)
     result = {
         'prediction': df['prediction'].to_list()
     }
+    
+    # Observe values
+    for pred in df['prediction'].to_list():
+        prediction_histogram.observe(float(pred))
     
     # Add accuracy if label in df and more than one row
     label_column = 'high_tip_indicator'
